f0b0ad412b8f23e5791ce1a74fa11f23ad32782a
Darryl Morris <dwsmorris@users.noreply.github.com>
2016-10-19 20:56:40 +0100

    initial files

---
+++ client/commonjs/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require("react-dom");
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _bazinga = require("./bazinga");
+
+var _bazinga2 = _interopRequireDefault(_bazinga);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var spread = function spread(first) {
+	for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+		rest[_key - 1] = arguments[_key];
+	}
+
+	return first.concat(rest);
+};
+
+_reactDom2.default.render(_react2.default.createElement(_bazinga2.default, null), document.getElementById("app-container"));
---
+++ client/jsx/app.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+import ReactDOM  from "react-dom";
+import Bazinga from "./bazinga";
+
+const spread = (first, ...rest) => first.concat(rest);
+
+ReactDOM.render(
+	<Bazinga/>,
+	document.getElementById("app-container")
+);
